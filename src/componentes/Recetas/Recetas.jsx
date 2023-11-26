a9e88d168b8d166c9469597b20888bd0826e1548
Angel Angeles <angel.angeles.molina@gmail.com>
2023-11-25 20:34:19 -0600

    responsive design

--- src/componentes/Recetas/Recetas.jsx
+++ src/componentes/Recetas/Recetas.jsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import BuscadorRecetas from './BuscadorRecetas/BuscadorRecetas';
+import BuscadorRecetas from "./BuscadorRecetas/BuscadorRecetas";
 
-import Alert from '@mui/material/Alert';
-import CircularProgress from '@mui/material/CircularProgress';
-import Grid from '@mui/material/Unstable_Grid2';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { pink } from '@mui/material/colors';
+import Alert from "@mui/material/Alert";
+import CircularProgress from "@mui/material/CircularProgress";
+import Grid from "@mui/material/Unstable_Grid2";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { pink } from "@mui/material/colors";
 
-import './Recetas.css';
+import "./Recetas.css";
 
 const Recetas = () => {
   const [muestraIndicadorCarga, estableceIndicadorCarga] = useState(true);
   const [errorCarga, estableceErrorCarga] = useState(null);
   const [recetas, estableceRecetas] = useState([]);
-  const [palabrasClave, establecePalabrasClave] = useState('');
+  const [palabrasClave, establecePalabrasClave] = useState("");
   const [tarjetasExpandidas, estableceTarjetaExpandida] = useState([]);
   const [favoritos, estableceFavoritos] = useState([]);
 
   const recuperaFavoritosGuardados = () => {
-    const favoritosGuardados = localStorage.getItem('NutriCook')
-      ? JSON.parse(localStorage.getItem('NutriCook'))
+    const favoritosGuardados = localStorage.getItem("NutriCook")
+      ? JSON.parse(localStorage.getItem("NutriCook"))
       : { favoritos: [] };
     return favoritosGuardados.favoritos;
   };
@@ -43,7 +43,7 @@ const Recetas = () => {
     }
     estableceFavoritos([...nuevosFavoritos]);
     localStorage.setItem(
-      'NutriCook',
+      "NutriCook",
       JSON.stringify({
         favoritos: [...nuevosFavoritos],
       })
@@ -67,7 +67,7 @@ const Recetas = () => {
           if (consulta.ok) {
             const recetasDeLaAPI = await consulta.json();
             if (!recetasDeLaAPI?.meals) {
-              estableceErrorCarga('No se encontraron resultados.');
+              estableceErrorCarga("No se encontraron resultados.");
             } else {
               estableceRecetas(recetasDeLaAPI.meals);
               recetasDeLaAPI.meals.map((receta) => {
@@ -75,13 +75,13 @@ const Recetas = () => {
                   .map(
                     (k) =>
                       receta[`strIngredient${k + 1}`] +
-                      ' (' +
+                      " (" +
                       receta[`strMeasure${k + 1}`] +
-                      ')'
+                      ")"
                   )
-                  .map((k) => k.replace(/\(([\s+]?\))/g, ''))
+                  .map((k) => k.replace(/\(([\s+]?\))/g, ""))
                   .filter((k) => k.trim().length > 0)
-                  .join(', ');
+                  .join(", ");
               });
               estableceFavoritos(recuperaFavoritosGuardados());
               estableceErrorCarga(null);
@@ -89,11 +89,11 @@ const Recetas = () => {
             estableceIndicadorCarga(false);
           } else {
             console.error(consulta.error);
-            estableceErrorCarga('No fue posible recuperar las recetas.');
+            estableceErrorCarga("No fue posible recuperar las recetas.");
           }
         } catch (error) {
           console.error(error);
-          estableceErrorCarga('Ocurrió un error inesperado.');
+          estableceErrorCarga("Ocurrió un error inesperado.");
         }
       })();
     }
@@ -108,15 +108,17 @@ const Recetas = () => {
     return (
       <>
         <Grid container className="contenedor">
-          <Grid xs={12} style={{ paddingBottom: '1.5rem' }}>
+          <Grid xs={12} style={{ paddingBottom: "1.5rem" }}>
             <BuscadorRecetas
-              procesaBusqueda={procesaBusqueda}></BuscadorRecetas>
+              procesaBusqueda={procesaBusqueda}
+            ></BuscadorRecetas>
           </Grid>
           <Grid
             xs={12}
             display="flex"
             justifyContent="center"
-            alignItems="center">
+            alignItems="center"
+          >
             <CircularProgress />
           </Grid>
         </Grid>
@@ -128,15 +130,17 @@ const Recetas = () => {
     return (
       <>
         <Grid container className="contenedor">
-          <Grid xs={12} style={{ paddingBottom: '1.5rem' }}>
+          <Grid xs={12} style={{ paddingBottom: "1.5rem" }}>
             <BuscadorRecetas
-              procesaBusqueda={procesaBusqueda}></BuscadorRecetas>
+              procesaBusqueda={procesaBusqueda}
+            ></BuscadorRecetas>
           </Grid>
           <Grid
             xs={12}
             display="flex"
             justifyContent="center"
-            alignItems="center">
+            alignItems="center"
+          >
             <Alert severity="error">{errorCarga}</Alert>
           </Grid>
         </Grid>
@@ -147,14 +151,28 @@ const Recetas = () => {
   return (
     <>
       <Grid container spacing={2} className="contenedor">
-        <Grid xs={12} style={{ paddingBottom: '1.5rem' }}>
+        <Grid xs={12} style={{ paddingBottom: "1.5rem" }}>
           <BuscadorRecetas procesaBusqueda={procesaBusqueda}></BuscadorRecetas>
         </Grid>
         <Grid container spacing={2} xs={12}>
           {recetas.map((receta) => (
-            <Grid xs={4} lg={3} key={receta.idMeal} className="tarjeta-receta">
-              <div>
-                <Card sx={{ maxWidth: 345 }}>
+            <Grid
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              key={receta.idMeal}
+              className="tarjeta-receta"
+            >
+              {/* <Grid xs={4} lg={3} key={receta.idMeal} className="tarjeta-receta"> */}
+              <div
+                style={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Card sx={{ maxWidth: 345, height: "100%" }}>
                   <CardHeader
                     title={receta.strMeal}
                     subheader={`Origen: ${receta.strArea}`}
@@ -173,35 +191,38 @@ const Recetas = () => {
                       {receta.ingredientes}.
                     </Typography>
                   </CardContent>
-                  <CardActions>
-                    <IconButton
-                      onClick={(e) =>
-                        procesaGuardadoFavorito(`${receta.idMeal}`, e)
-                      }
-                      aria-label="agregar a favoritos">
-                      <FavoriteIcon
-                        sx={{
-                          color: favoritos.includes(receta.idMeal)
-                            ? pink[500]
-                            : null,
-                        }}
-                      />
-                    </IconButton>
-                    <div style={{ flex: '1 1 auto' }}></div>
-                    <Button
-                      onClick={(e) =>
-                        procesaExpansionTarjeta(`${receta.idMeal}`, e)
-                      }
-                      aria-label="mostrar más">
-                      {!tarjetasExpandidas.includes(`${receta.idMeal}`)
-                        ? 'Mostrar instrucciones'
-                        : 'Ocultar instrucciones'}
-                    </Button>
-                  </CardActions>
+                    <CardActions>
+                      <IconButton
+                        onClick={(e) =>
+                          procesaGuardadoFavorito(`${receta.idMeal}`, e)
+                        }
+                        aria-label="agregar a favoritos"
+                      >
+                        <FavoriteIcon
+                          sx={{
+                            color: favoritos.includes(receta.idMeal)
+                              ? pink[500]
+                              : null,
+                          }}
+                        />
+                      </IconButton>
+                      <div style={{ flex: "1 1 auto" }}></div>
+                      <Button
+                        onClick={(e) =>
+                          procesaExpansionTarjeta(`${receta.idMeal}`, e)
+                        }
+                        aria-label="mostrar más"
+                      >
+                        {!tarjetasExpandidas.includes(`${receta.idMeal}`)
+                          ? "Mostrar instrucciones"
+                          : "Ocultar instrucciones"}
+                      </Button>
+                    </CardActions>
                   <Collapse
                     in={tarjetasExpandidas.includes(`${receta.idMeal}`)}
                     timeout="auto"
-                    unmountOnExit>
+                    unmountOnExit
+                  >
                     <CardContent>
                       <Typography variant="body1" gutterBottom>
                         Instrucciones
